549ae91729e05e35cc1a7e5b6ef883957a87fd99
imdongjin <geniuslim27@gmail.com>
2019-09-21 07:30:58 +0900

    refactoring: some refactoring, write readme

--- app.js
+++ app.js
@@ -26,13 +26,13 @@ router.use(bodyParser.urlencoded({ extended: true }))
 router.use(awsServerlessExpressMiddleware.eventContext())
 
 // NOTE: tests can't find the views directory without this
-app.set('views', path.join(__dirname, 'views'))
+// app.set('views', path.join(__dirname, 'views'))
 
-router.get('/', (req, res) => {
-  res.render('index', {
-    apiUrl: req.apiGateway ? `https://${req.apiGateway.event.headers.Host}/${req.apiGateway.event.requestContext.stage}` : 'http://localhost:3000'
-  })
-})
+//router.get('/', (req, res) => {
+//  res.render('index', {
+//    apiUrl: req.apiGateway ? `https://${req.apiGateway.event.headers.Host}/${req.apiGateway.event.requestContext.stage}` : 'http://localhost:3000'
+//  })
+//})
 
 var params = {TableName: 'bills', Limit: 1000, AttributesToGet: ['id']}
 
@@ -66,11 +66,17 @@ router.get('/bills', (req, res) => {
       res.json({result: data})
   })
 })
+router.get('/', (req, res) => {
+  dynamoDb.scan(params, function scanUntilDone(error, data) {
+    if (error) {
+      res.status(400).json({ error: error})
+      return true
+    }
+      res.json({result: data})
+  })
+})
 
-// The aws-serverless-express library creates a server and listens on a Unix
-// Domain Socket for you, so you can remove the usual call to app.listen.
-// app.listen(3000)
+
 app.use('/', router)
 
-// Export your express server so you can import it in the lambda function.
 module.exports = app
